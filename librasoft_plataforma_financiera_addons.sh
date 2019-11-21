1ad6fbca3ae1bdc81a39fd01eed921ce4cf45e9f
Levis Libra bagho <levislibra@hotmail.com>
2019-11-21 09:40:01 -0300

    first commit

---
+++ librasoft_plataforma_financiera_addons.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/levislibra/financiera_prestamos.git
+git clone https://github.com/levislibra/financiera_pagos_360.git
+git clone https://github.com/levislibra/financiera_comercio.git
+git clone https://github.com/levislibra/financiera_bna_debito_automatico.git
+git clone https://github.com/levislibra/financiera_base.git
+git clone https://github.com/levislibra/financiera_comision.git
+git clone https://github.com/levislibra/financiera_cobranza_mora.git
+git clone https://github.com/levislibra/feriados.git
